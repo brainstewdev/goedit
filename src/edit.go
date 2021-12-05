59b1c684e5a50b73cd024d7b96da2021e38581c3
Lorenzo <brainstew927@gmail.com>
2021-12-05 20:50:44 +0100

    funzionante tranne per problema sovrascrittura slice da append

--- src/edit.go
+++ src/edit.go
@@ -27,6 +27,13 @@ func LeggiLinee(in io.Reader)  []string {
 	}
 	return text_lines
 }
+
+func LeggiLinea(in io.Reader) string{
+	scanner := bufio.NewScanner(in)
+	scanner.Scan()
+	return  scanner.Text()
+}
+
 func IndiceRiga(num_righe int) int {
 	var inp_ind string
 
@@ -56,12 +63,48 @@ func IndiceRiga(num_righe int) int {
 	} 
 }
 
+func IsStringaNumero(s string) bool {
+	for _,v := range []rune(s) {
+		if !unicode.IsNumber(v){
+			return false
+		}
+	}
+	return true
+}
+
+
+
+func PrintLines(lines []string){
+	// stampo riga per riga con l'indice a sinistra
+	for i:=0; i < len(lines); i++{
+		fmt.Print(i, "\t: ", lines[i] + "\n")
+	}
+}
+
 func ScriviSlice(writer *bufio.Writer, linee []string){
 	for _,v := range linee{
 		fmt.Println([]byte(v))
 		fmt.Fprint(writer, v)
 	}
 	writer.Flush()
+}
+
+func AggiungiLinea(linee []string, lineaDaAggiungere string, pos int)[]string{
+	// fmt.Println("ATTENZIONE ATTENZIONE! ")
+	var temp []string
+	prima_parte := linee[:pos]
+	seconda_parte := linee[pos:]
+	fmt.Println("Analisi secondo Append: ",linee)
+	// mette in temp linee da 0 a pos-1 + lineaDaAggiungere
+	temp = append(prima_parte, lineaDaAggiungere)
+	fmt.Println("PRIMO APPEND", temp)
+	// mette in temp linee da 0 a pos-1 + lineaDaAggiungere + 
+	temp = append(temp, seconda_parte...)
+	fmt.Println("Analisi secondo Append: ",linee)
+	fmt.Println("secondo APPEND", temp)
+	
+	fmt.Println("lunghezza di temp:", len(temp))
+	return temp
 }
 
 func main(){
@@ -82,25 +125,52 @@ func main(){
 	var lines []string
 	lines = LeggiLinee(f)
 	// ora ho letto tutte le linee
-	// pulisco lo schermo e scrivo a schermo tutte le linee
-	fmt.Print("\033[H\033[2J")
-	// stampo riga per riga con l'indice a sinistra
-	for i:=0; i < len(lines); i++{
-		fmt.Print(i, "\t: ", lines[i] + "\n")
-	}
-	// prendo in input l'indice della riga che voglio modificare
-	var indice int
-	indice = IndiceRiga(len(lines))
-	fmt.Println("Insert the new value for line", indice, ":")
-	// ora ho all'interno di indice la riga voluta:
-	// leggo da input del testo (con la funz LeggiTesto)
-	// e lo metto all'interno dell'elemento di indice indice della slice
+	var quit bool
+	// CICLO OPERAZIONI PARTE DA QUA
+	for !quit {
+		// pulisco lo schermo e scrivo a schermo tutte le linee
+		// fmt.Print("\033[H\033[2J")
+		PrintLines(lines)
+		// prendo il comando in input
+		/*
+			comandi disponibili: 
+				i x n -> con n e x numero 
+					inserisce n linee vuote dopo la posizione x
+		*/
+		// prendo in input l'indice della riga che voglio modificare
+		var com string
+		fmt.Print(":")
+		com = LeggiLinea(os.Stdin)
+		switch {
+		case []rune(com)[0] == 'i':
+			// devo controllare che vi siano x e n
+			cmd_args := strings.Split(com, " ")
+			fmt.Println("dentro il case switch: ", cmd_args)
+			
+			if len(cmd_args) == 3 && IsStringaNumero(cmd_args[1]) && IsStringaNumero(cmd_args[2]){
+				// prendo i numeri come interi per comodità
+				var x, n int
+				x, _ = strconv.Atoi(cmd_args[1])
+				n, _= strconv.Atoi(cmd_args[2])
+				// eseguo il comando
+				for i:= 0; i < n; i++{
+					lines = AggiungiLinea(lines, " ", x)
+				}
+			}
+		case []rune(com)[0] == 'q':
+			quit = true
+			
+		}
+		fmt.Println(lines)
+		// fmt.Println("Insert the new value for line", indice, ":")
+		// ora ho all'interno di indice la riga voluta:
+		// leggo da input del testo (con la funz LeggiTesto)
+		// e lo metto all'interno dell'elemento di indice indice della slice
 
-	stringa_input := JoinLinee(LeggiLinee(os.Stdin))
-	lines[indice] = stringa_input
-	for _, v := range lines {
-		fmt.Println(v)
+		// stringa_input := JoinLinee(LeggiLinee(os.Stdin))
+		// lines[indice] = stringa_input
 	}
+	// CICLO OPERAZIONI FINISCE QUA
 
 	f.Close()
 	// riapro file in scrittura stavolta
