bffcecdad8629b410e089fc7460471cefa2e1d63
Lorenzo <brainstew927@gmail.com>
2021-12-14 19:52:44 +0100

    attenzione: commit per backup, non funziona

---
+++ src/utility/types.go
@@ -0,0 +1,15 @@
+package utility
+
+type importantWords_type struct {
+	Types    []string
+	Keywords []string
+}
+type scheme_type struct {
+	Background		string
+	Base            string
+	Reset           string
+	Keywords        string
+	Types           string
+	Numbers         string
+	StringsLiterals string
+}
